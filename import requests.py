ac0cb26c7f2fc5a1bef6c6680005a981204dfdc8
Yağmur Uçar <yagnurucar@gmail.com>
2023-07-22 13:49:26 +0300

    main.python

---
+++ import requests.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+gelen_cevap= requests.get("http://api.open-notify.org/astros.json")
+
+uzaydaki_insan_sayisi =gelen_cevap.json()["number"]
+
+print(f"Şu anda uzayda {uzaydaki_insan_sayisi} insan var")
+
+print("uzaydaki insanların isimleri")
+print("****************")
+
+for kisi in gelen_cevap.json()["people"]:
+    print(kisi["name"]," --->",kisi["craft"])
